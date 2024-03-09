c5cc70e2ce5aac7b013f0419056875b48ae42659
Anurag Jawalkar <130534473+anuragjawalkar@users.noreply.github.com>
2024-03-09 08:42:56 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (49.18%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -14,44 +14,25 @@
  * }
  */
 class Solution {
-    public void recoverTree(TreeNode root) {
-        TreeNode prev = null;
-        TreeNode current = root;
-        TreeNode first = null;
-        TreeNode second = null;
-        while (current != null) {
-            if (current.left != null) {
-                TreeNode temp = current.left;
-                while (temp.right != null && temp.right != current) {
-                    temp = temp.right;
-                }
-                if (temp.right == null) {
-                    temp.right = current;
-                    current = current.left;
-                } else {
-                    temp.right = null;
-                    if (prev != null && prev.val > current.val) {
-                        if (first == null) {
-                            first = prev;
-                        }
-                        second = current;
-                    }
-                    prev = current;
-                    current = current.right;
-                }
-            } else {
-                if (prev != null && prev.val > current.val) {
-                    if (first == null) {
-                        first = prev;
-                    }
-                    second = current;
-                }
-                prev = current;
-                current = current.right;
-            }
+    TreeNode prev=null,first=null,second=null;
+    void inorder(TreeNode root){
+        if(root==null)
+            return ;
+        inorder(root.left);
+        if(prev!=null&&root.val<prev.val){
+            if(first==null)
+                first=prev;
+            second=root;
         }
-        int temp = first.val;
-        first.val = second.val;
-        second.val = temp;
+        prev=root;
+        inorder(root.right);
+    }
+    public void recoverTree(TreeNode root) {
+        if(root==null)
+            return ; 
+        inorder(root);
+        int temp=first.val;
+        first.val=second.val;
+        second.val=temp;
     }
 }
